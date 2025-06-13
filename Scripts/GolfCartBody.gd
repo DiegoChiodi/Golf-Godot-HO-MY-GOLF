0afec91e9be77b5583049c611a129c6677a851cc
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-06-13 19:05:01 -0300

    golf cart moving add

--- Scripts/GolfCartBody.gd
+++ Scripts/GolfCartBody.gd
@@ -1,14 +1,31 @@
 extends CharacterBody2D
 
-const player := preload("res://Scripts/PlayerBody.gd")
+@onready var player := get_node("../Player")  # Ajuste o caminho conforme necessário
 
-
-var entered_body_id : int = -1  # -1 significa "vazio/nada entrou"
+var toRide = false
+const speed = 50
 func _ready():
 	pass
 func _physics_process(delta: float) -> void:
-	pass    
+	if toRide:
+		driving(delta)
+		player.position.x = position.x
+		player.position.y = position.y
+		
+		
+func driving(delta: float) -> void:
+	var up_keep = Input.is_action_pressed("move_up")
+	var down_keep = Input.is_action_pressed("move_down")
+	var left_keep = Input.is_action_pressed("move_left")
+	var right_keep = Input.is_action_pressed("move_right")
 
-
-func _on_intercat_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if right_keep:
+		rotation_degrees += 90 * delta
+	if left_keep:
+		rotation_degrees -= 90 * delta
+	if up_keep:
+		var direction = Vector2.RIGHT.rotated(deg_to_rad(rotation_degrees))
+		position += direction * speed * delta
+	if down_keep:
+		var direction = Vector2.LEFT.rotated(deg_to_rad(rotation_degrees))
+		position -= direction * speed * delta
--- Scripts/IntercatArea.gd
+++ Scripts/IntercatArea.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+var colPlayer = false;
+
+func _process(delta):
+	if Input.is_action_just_pressed("move_space") && colPlayer:
+		print("Ação direita pressionada")
+		get_parent().toRide = true
+	
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
-		print("Acertou o player!")
+		colPlayer = true;
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		colPlayer = false;
+		
