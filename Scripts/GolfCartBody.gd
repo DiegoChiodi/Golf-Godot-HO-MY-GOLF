c4d686d02a915b5b283de295c55c557cbb3a75b9
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-06-16 21:12:05 -0300

    GolfCart direction add and debug

--- Scripts/GolfCartBody.gd
+++ Scripts/GolfCartBody.gd
@@ -7,19 +7,12 @@ var rotation_speed = 2.5
 var toRide = false
 const speed = 1
 var exit = false
-
 @onready var spr := $spr_golfCart as Sprite2D
 @onready var lbl_debug: Label = $lbl_debug
+var fatAngle = 360 / 8#45
+var compAngle = fatAngle / 2
 
-enum State {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT
-}
-var state = State.RIGHT
-
-@onready var angle = 0
+var angle = 0
 
 func _process(delta: float) -> void:
 	rotation_degrees = int(rotation_degrees) % 360
@@ -27,7 +20,26 @@ func _process(delta: float) -> void:
 	if (0 > angle):
 		angle += 360
 	
-	lbl_debug.text = "%.2f" % angle  # Ex.: "90.00" ou "45.50"
+	if fatAngle * 7 + compAngle < angle || angle < fatAngle * 0 + compAngle:
+		lbl_debug.text = "direita"
+	elif compAngle < angle && angle < fatAngle * 1 + compAngle:
+		lbl_debug.text = "direita baixo"
+	elif fatAngle + compAngle < angle && angle < fatAngle * 2 + compAngle:
+		lbl_debug.text = "baixo"
+	elif fatAngle * 2 + compAngle < angle && angle < fatAngle * 3 + compAngle:
+		lbl_debug.text = "esquerda baixo"
+	elif fatAngle * 3 + compAngle < angle && angle < fatAngle * 4 + compAngle:
+		lbl_debug.text = "esquerda"
+	elif fatAngle * 4 + compAngle < angle && angle < fatAngle * 5 + compAngle:
+		lbl_debug.text = "esquerda cima"
+	elif fatAngle * 5 + compAngle < angle && angle < fatAngle * 6 + compAngle:
+		lbl_debug.text = "cima"
+	elif fatAngle * 6 + compAngle < angle && angle < fatAngle * 7 + compAngle:
+		lbl_debug.text = "direita cima"
+	
+	lbl_debug.rotation = -rotation
+	lbl_debug.position = Vector2(0, 0)
+		
 	
 func _physics_process(delta: float) -> void:
 	if toRide:
