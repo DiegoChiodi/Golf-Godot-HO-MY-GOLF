05776f6465396bbf757cdc8ff2ce23907bc246c3
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-07-08 22:50:19 -0300

    GDD shot for break scene

--- Scripts/Hand.gd
+++ Scripts/Hand.gd
@@ -3,10 +3,8 @@ extends Sprite2D
 @export var hand : int
 @onready var golfClub = get_parent().get_node("bod_golfClub")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	z_index = golfClub.z + hand
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = Vector2.DOWN.rotated(golfClub.rotation) * (10 + hand)
