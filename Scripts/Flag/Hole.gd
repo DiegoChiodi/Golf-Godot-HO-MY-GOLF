1526f0fca78230aec8fd57134698cd81b4322ec0
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-08-20 15:50:23 -0300

    colision ball-hole

--- Scripts/Flag/Hole.gd
+++ Scripts/Flag/Hole.gd
@@ -13,6 +13,6 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var areFather = area.get_parent()
-	if areFather.is_in_group("ball") and areFather.posZ <= 2 and area.is_in_group("colHb") and areFather.speed > Vector2(2,2): 
+	if areFather.is_in_group("ball") and areFather.posZ <= 4 and area.is_in_group("colHb") and areFather.speed.length() > 3: 
 		game_manager.finishWorld()
 		print('wow owow woowo')
